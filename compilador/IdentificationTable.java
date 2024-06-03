e82895cd4a40d27762d43ac1af756444cc32e9fa
GersonMacedo <grsnvinicius24@gmail.com>
2024-06-03 19:17:58 -0300

    Implementa IdentificationTable

--- compilador/IdentificationTable.java
+++ compilador/IdentificationTable.java
@@ -1,5 +1,18 @@
 package compilador;
 
-public class IdentificationTable {
+import java.util.Map;
 
+import compilador.ast.Declaracao;
+
+public class IdentificationTable {
+    Map<String, Declaracao> varMap;
+
+    public Declaracao add(Declaracao d){
+        Declaracao previousValue = varMap.put(d.i.n, d);
+        return previousValue;
+    }
+
+    public Declaracao get(String s){
+        return varMap.get(s);
+    }
 }
